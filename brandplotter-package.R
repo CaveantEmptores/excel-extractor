cf9c51274a4aa0234d4e854dd0e29e59f095c30c
CaveantEmptores <161971808+CaveantEmptores@users.noreply.github.com>
2025-09-06 18:26:11 -0400

    Create brandplotter-package.R

---
+++ brandplotter-package.R
@@ -0,0 +1,10 @@
+# file: R/brandplotter-package.R
+
+#' brandplotter: branded, responsive ggplots from long/tall data
+#'
+#' Tools to render common business charts (clustered/stacked bars, lines, and combos)
+#' from normalized long-format data with a consistent brand theme and palettes.
+#'
+#' @docType package
+#' @name brandplotter
+NULL
